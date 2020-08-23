ef2626439ae66f2f499c12b7c48e2b645247e8ee
경두현 <douhyun.kyung@gmail.com>
2020-08-23 23:56:26 +0900

    :twisted_rightwards_arrows: "Cal-Culator-Mobile-Server"_Update Dev Branch
    - Dev 브랜치 업데이트 #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,17 @@
 const pkg = require('./package.json');
 
 module.exports = {
-  extends: ['prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-  },
-  env: {
-    es6: true,
-  },
+	extends: ['prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-unused-vars': 'warn',
+		'no-console': 'off',
+	},
+	parserOptions: {
+		ecmaVersion: 2017,
+	},
+	env: {
+		es6: true,
+	},
 };
