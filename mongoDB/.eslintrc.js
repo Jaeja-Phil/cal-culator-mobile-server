8637b5c4c5e9d53ebcfa3b5c945f49510ebf2b6b
경두현 <douhyun.kyung@gmail.com>
2020-08-25 16:22:43 +0900

    :construction: mongoDB/_Add GraphQL, Mongoose 설정
    - mongoDB 폴더 생성 - models.js, resolvers.js, typeDefs.js 파일 생성 - user, food, fooduser의 typeDefs, model의 설정 완료 - user, food의 추가 resolvers 설정 완료 - 모듈 추가 설정 변경 ( require -> import )

---
+++ mongoDB/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: ['prettier'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-unused-vars': 'warn',
+		'no-console': 'off',
+	},
+	parserOptions: {
+		ecmaVersion: 2017,
+	},
+	env: {
+		es6: true,
+	},
+};
---
+++ mongoDB/models.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+export const User = mongoose.model('User', {
+	name: String,
+	password: String,
+	email: String,
+	gender: String,
+	age: Number,
+	created_At: Date,
+});
+
+export const Food = mongoose.model('Food', {
+	name: String,
+	calorie: Number,
+	fat: Number,
+	carbohydrate: Number,
+	sugar: Number,
+	protein: Number,
+	sodium: Number,
+	chloesterol: Number,
+	iron: Number,
+	calcium: Number,
+	zinc: Number,
+	vitamin_a: Number,
+	vitamin_b: Number,
+	vitamin_c: Number,
+	created_At: Date,
+});
+
+export const FoodUser = mongoose.model('FoodUser', {
+	amout: Number,
+	time: String,
+	user_id: Number,
+	food_id: Number,
+	created_At: Date,
+});
